0f86ca572226fda1ea3658ad7e9d00f9c3fdd769
IchMayer <43932918+IchMayer@users.noreply.github.com>
2019-03-27 08:08:32 +0300

    Report 2lab

--- test/test_lex_analyzer.cpp
+++ test/test_lex_analyzer.cpp
@@ -3,7 +3,7 @@
 #include <lex_analyzer.h>
 
 int main() {
-	std::string file = "struct a\n{\n\tfloat b;\n};\nint main()\n{\n\tint c;\n\tc = 5+6*(1/3);\n\t float d;\n\ta t;\n\tt.b = 0.5;\n\td = 1564*15.15 - 0.6464;\n\te = c != d - c == d;\n\t/* aoeusnthoaeu float: a*b */\n\td = d;\n\treturn 0;\n}";
+	std::string file = "struct a\n{\n\tfloat b;\n};\nint main()\n{\n\tint c;\n\tc = 5+6*(1/3);\n\t float d;\n\ta t;\n\tt.b. = 0.5;\n\td = 1564*15.15 - 0.6464;\n\te = c != d - c == d;\n\t/* aoeusnthoaeu float: a*b */\n\td = d;\n\treturn 0;\n}";
 	file += ' ';
 
 	std::cout << "Parsing of string: \n" << file << std::endl;
