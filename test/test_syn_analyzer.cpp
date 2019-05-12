b75f2ae39601ace16bdd62607d7d7f2e7a06981b
IchMayer <43932918+IchMayer@users.noreply.github.com>
2019-05-12 19:29:56 +0300

    3 лаба
    Постфиксный обход сделан через стек

--- test/test_syn_analyzer.cpp
+++ test/test_syn_analyzer.cpp
@@ -4,7 +4,7 @@
 #include <syn_analyzer.h>
 
 int main() {
-	std::string file = "struct A\n{\n\tfloat b;\n\tint d;\n};\nstruct B\n{\n\tint c;\n};\nvoid main()\n{\n\tint c;\n\tc = (5+6)*4;\n\tfloat d;\n\tA t, o;\n\tB i;\n\tt.b = 0.5;\n\to = t;\n\to = i;\n\td = 1564*15.15 - 0.6464;\n\tint e = c != d - c == d;\n\t/* aoeusnthoaeu float: a*b **/\n\td= d;\n}";
+	std::string file = "struct A\n{\n\tfloat b;\n\tint d;\n};\nstruct B\n{\n\tint c;\n};\nvoid main()\n{\n\tint c;\n\tc = (5+6)*4 + 7 * 65.3;\n\tint d;\n\tA t, o;\n\tB i;\n\tt.b = 0.5;\n\to = t;\n\t//o = i;\n\td = 1564*15.15 - 0.6464;\n\tint e = c != d - c == d;\n\t/* aoeusnthoaeu float: a*b **/\n\td= d;\n}";
 	file += ' ';
 
 	std::cout << "Parsing of string: \n" << file << std::endl;
@@ -17,8 +17,14 @@ int main() {
 		string path = "syn_table.txt";
 
 		auto result = Analyzer(tokens, tables, path);
+		std::cout << std::endl <<  "OK, syn analyzer" << std::endl << std::endl;
 
-		std::cout << "OK, syn analyzer" << std::endl;
+		for (auto& i : result)
+		{
+			for (auto& j : i)
+				std::cout << tables.getStr(j) << " ";
+			std::cout << std::endl;
+		}
 	}
 	catch (string &e)
 	{
